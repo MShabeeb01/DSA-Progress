ebb8c4b831cc7bc3201a777e0e6d266ffcbaca60
Mohammed Shabeeb <mohammed.shabeebbb@gmail.com>
2025-10-01 16:51:08 +0530

    Taking user input and displaying the output.

---
+++ 04_Strings/02_User_Input_Output.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ArraysCC{
+    public static void main(String args[]){
+        System.out.print("Enter your name:");
+        Scanner sc = new Scanner(System.in); // Creating a scanner class.
+        String name = sc.nextLine(); //Taking user input.
+        System.out.println("Your name is :" + name); // Output.
+    }
+}
