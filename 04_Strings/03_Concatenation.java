8e62ee35331502737df34bae7cfbb9337c97a276
Mohammed Shabeeb <mohammed.shabeebbb@gmail.com>
2025-10-01 16:55:18 +0530

    Joining Two Strings Through concatenation.

---
+++ 04_Strings/03_Concatenation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ArraysCC{
+    public static void main(String args[]){
+        String firstname = "Mohammed";
+        String lastname = "Shabeeb";
+        String Fullname = firstname +" " + lastname; //Concatenation :Joining two strings.
+        System.out.println(Fullname);
+    }
+}
