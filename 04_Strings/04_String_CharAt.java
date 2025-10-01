ddfd7df2e9db63d410f24264cf264d7ded4a1d67
Mohammed Shabeeb <mohammed.shabeebbb@gmail.com>
2025-10-01 16:58:15 +0530

    Finding character at specific index.

---
+++ 04_Strings/04_String_CharAt.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ArraysCC{
+    public static void main(String args[]){
+    String name = "Shabeeb";
+    System.out.println(name.charAt(2)); // Character at index 2 (3rd position.) = a
+    }
+}
