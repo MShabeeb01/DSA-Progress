048c8ae98ab8fa9a9fab8027ca07120488b8481d
Mohammed Shabeeb <mohammed.shabeebbb@gmail.com>
2025-10-01 16:43:16 +0530

    Declaring  a String.

---
+++ 04_Strings/01_Declaring_String.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ArraysCC{
+    public static void main(String args[]){
+        String str = "Shabeeb";
+        String st = new String("Henry");
+        // These are the two methods to declare a string.
+        System.out.print(st);
+    }
+}
