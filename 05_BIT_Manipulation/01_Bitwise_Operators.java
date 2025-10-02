dba35dc511a63dc656427a859b822f1491baaf3c
Mohammed Shabeeb <mohammed.shabeebbb@gmail.com>
2025-10-02 12:23:39 +0530

    Bitwise Operators.

---
+++ 05_BIT_Manipulation/01_Bitwise_Operators.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class BitManipulation{
+    public static void main(String args[]){
+    System.out.println(5 & 6); // Binary AND(&) Operator.
+    System.out.println(5 | 6); // Binary OR(|) Operator.
+    System.out.println(5 ^ 6); // Binary XOR(^) Operator.
+    System.out.println(~5); //Binary one's Compliment (NOT (~)) Operator.
+    System.out.println(5<<2); // Binary Left Shift(<<) Operator.
+    System.out.println(6>>1);// Binary Right Shift (>>)  Operator.
+
+    }
+}
